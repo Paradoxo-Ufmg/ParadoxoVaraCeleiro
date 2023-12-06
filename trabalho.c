0c3ab764986e804fcc3d02965fc697a9d8fde925
Paradoxo-Ufmg <152902422+Paradoxo-Ufmg@users.noreply.github.com>
2023-12-06 10:59:12 -0300

    trabalho.c

--- trabalho.c
+++ trabalho.c
@@ -2,11 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Função principal
+ * Está função pede ao usuario dois valores, para o tamanho da vara e o outro para o tamanho do celeiro.
+ * Com base nesses valores é chamada as funções que foram criadas para o trabalho e elas são executadas.
+ * 
+ * @return 0
+ */
+
 int main() {
     // Definindo as dimensões dos objetos
 
-    int tamanhoVara = 16;
-    int tamanhoCeleiro = 10;
+    double tamanhoVara;
+    double tamanhoCeleiro;
+
+    do {
+
+        printf("Escreva o tamanho da Vara: ");
+        scanf("%lf", &tamanhoVara);
+
+        printf("Escreva o tamanho do celeiro: ");
+        scanf("%lf", &tamanhoCeleiro);
+
+    } while (tamanhoVara < tamanhoCeleiro);
 
     // Calculando os valores 
     double expVelocidade = expressaoVelocidade(tamanhoCeleiro, tamanhoVara);
